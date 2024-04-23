5b16a492e5238a3c3273bb99540b9d52d6e2ba88
Syazwani MZ <syazwanimdzin@gmail.com>
2024-04-23 22:13:26 +0800

    Update ball.lua
    Cleaning up comments

--- ball.lua
+++ ball.lua
@@ -34,25 +34,25 @@ function Ball:collide()
 
     -- CHECK TOP AND BOTTOM BOUNDARY
     if self.y < 0 then
-        -- check if the ball hit the top boundary
+        -- if the ball hit the top boundary
         self.y = 0
         self.yVelocity = -self.yVelocity
         
     elseif self.y + self.height > love.graphics.getHeight() then
-        -- check if the ball hit the bottom boundary
+        -- if the ball hit the bottom boundary
         self.y = love.graphics.getHeight() - self.height
         self.yVelocity = -self.yVelocity
     end
 
     -- If outside boundary, send to center
-    if self.x < 0 then -- check if the ball hit left boundary
+    if self.x < 0 then -- if the ball hit left boundary
         self.x = love.graphics.getWidth() / 2 - self.width /2
         self.y = love.graphics.getHeight() / 2 - self.height/2
         self.yVelocity = 0
         self.xVelocity = self.speed
     end
 
-    -- check if the ball hit right boundary then send ball to player
+    -- if the ball hit right boundary then send ball to player
     if self.x > love.graphics.getWidth() then 
         self.x = love.graphics.getWidth() / 2 - self.width /2
         self.y = love.graphics.getHeight() / 2 - self.height/2
